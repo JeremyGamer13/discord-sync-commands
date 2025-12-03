2d99d71194febbc8772cfa72f83c1ac75f48fdf7
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-12-02 20:43:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const Discord = require('discord.js');
 
+/**
+ * @typedef SyncOptions
+ * @property {boolean} debug Whether to log actions or not
+ * @property {string} guildId Only register commands for a single guild.
+ */
+/**
+ * @typedef SyncResults
+ * @property {number} currentCommandCount
+ * @property {number} newCommandCount
+ * @property {number} deletedCommandCount
+ * @property {number} updatedCommandCount
+ */
+/**
+ * @param {Discord.Client} client 
+ * @param {[Discord.ApplicationCommandDataResolvable]} commands 
+ * @param {SyncOptions} options 
+ * @returns {SyncResults}
+ */
 module.exports = async (client, commands, options = {
     debug: false,
     guildId: null
@@ -9,24 +27,36 @@ module.exports = async (client, commands, options = {
 
     const ready = client.readyAt ? Promise.resolve() : new Promise(resolve => client.once('ready', resolve));
     await ready;
-    const currentCommands = await client.application.commands.fetch(options.guildId && { guildId: options.guildId });
+
+    let currentCommands;
+    if (options.guildId) {
+        const guild = client.guilds.cache.get(options.guildId);
+        if (!guild) throw new Error(`Guild ${options.guildId} not found`);
+        currentCommands = await guild.commands.fetch();
+    } else {
+        currentCommands = await client.application.commands.fetch();
+    }
 
     log(`Synchronizing commands...`);
     log(`Currently ${currentCommands.size} commands.`);
 
     const newCommands = commands.filter((command) => !currentCommands.some((c) => c.name === command.name));
     for (let newCommand of newCommands) {
-        await client.application.commands.create(newCommand, options.guildId);
+        if (options.guildId) {
+            await client.guilds.cache.get(options.guildId).commands.create(newCommand);
+        } else {
+            await client.application.commands.create(newCommand);
+        }
     }
 
     log(`Created ${newCommands.length} commands!`);
 
-    const deletedCommands = currentCommands.filter((command) => !commands.some((c) => c.name === command.name)).toJSON();
+    const deletedCommands = currentCommands.filter((command) => !commands.some((c) => c.name === command.name));
     for (let deletedCommand of deletedCommands) {
         await deletedCommand.delete();
     }
 
-    log(`Deleted ${deletedCommands.length} commands!`);
+    log(`Deleted ${deletedCommands.size} commands!`);
 
     const updatedCommands = commands.filter((command) => currentCommands.some((c) => c.name === command.name));
     let updatedCommandCount = 0;
@@ -49,7 +79,7 @@ module.exports = async (client, commands, options = {
     return {
         currentCommandCount: currentCommands.size,
         newCommandCount: newCommands.length,
-        deletedCommandCount: deletedCommands.length,
+        deletedCommandCount: deletedCommands.size,
         updatedCommandCount
     };
 
